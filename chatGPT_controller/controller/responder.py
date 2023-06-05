58bcf8ea87f271ed9f1dd680bdd373e98c18e975
Sekiro <1326555262@qq.com>
2023-06-05 17:40:13 +0800

    变更

--- chatGPT_controller/controller/responder.py
+++ chatGPT_controller/controller/responder.py
@@ -10,6 +10,8 @@ openai.api_base = 'https://service-i501wcby-1318284291.jp.apigw.tencentcs.com/v1
 # 主GPT函数，接收一个字符串，返回由GPT生成的字符串
 def gpt_responder(user_content):
     # content是一个字符串，是用户输入的内容
+    # 截断user_content，只保留1500个字符
+    user_content = user_content[:1500]
     response = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
         messages=[
